93a183748e17c65f6342f401bf3206b0a40383c9
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-08 16:05:17 +0700

    Menambahkan route read, update id

--- project-crud/src/App.jsx
+++ project-crud/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Write from "./components/Write";
+import Read from "./components/Read";
+import Update from "./components/Update";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Write />} />
+          <Route path="/" element={<Read />} />
           <Route path="/write" element={<Write />} />
+          <Route path="/read" element={<Read />} />
+          <Route path="/update/:id" element={<Update />} />
         </Routes>
       </Router>
     </>
