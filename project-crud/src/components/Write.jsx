a1f74e415ba5afee936344babe6341ed2a7888ba
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-08 13:54:59 +0700

    Membuat halaman write

---
+++ project-crud/src/components/Write.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Write = () => {
+  return (
+    <div className="bg-fuchsia-700 p-40">
+      <h1 className="text-red-800 font-extrabold text-white">hello </h1>
+    </div>
+  );
+};
+
+export default Write;
